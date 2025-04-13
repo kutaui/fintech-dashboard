69faf34bba99299f945c416c4244e9e294cf91e1
Kutay <kutaybekleric@gmail.com>
2025-04-13 13:29:14 +0300

    feat: mainsidebar component

---
+++ src/components/MainSidebar.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarHeader,
+	SidebarInset,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarProvider,
+	SidebarTrigger,
+} from '@/components/ui/sidebar'
+import { Home, Settings, User } from 'lucide-react'
+import Link from 'next/link'
+
+interface MainSidebarProps {
+	children?: React.ReactNode
+}
+
+export default function MainSidebar({ children }: MainSidebarProps) {
+	return (
+		<SidebarProvider>
+			<Sidebar>
+				<SidebarHeader className="p-4 flex items-center justify-between">
+					<h2 className="font-bold text-xl">FinSureTex</h2>
+				</SidebarHeader>
+				<SidebarContent>
+					<SidebarMenu>
+						<SidebarMenuItem>
+							<SidebarMenuButton asChild>
+								<Link href="/">
+									<Home />
+									<span>Home</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+						<SidebarMenuItem>
+							<SidebarMenuButton asChild>
+								<Link href="/profile">
+									<User />
+									<span>Profile</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+						<SidebarMenuItem>
+							<SidebarMenuButton asChild>
+								<Link href="/settings">
+									<Settings />
+									<span>Settings</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					</SidebarMenu>
+				</SidebarContent>
+			</Sidebar>
+			<SidebarInset>
+				<div className="p-4">
+					<SidebarTrigger />
+					{children}
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
+	)
+}
