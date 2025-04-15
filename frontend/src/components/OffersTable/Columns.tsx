088bdeb7f94682c29b382b6169ceb34c4aa82133
Kutay <kutaybekleric@gmail.com>
2025-04-15 23:19:28 +0300

    feat: cleanup column action cell

--- frontend/src/components/OffersTable/Columns.tsx
+++ frontend/src/components/OffersTable/Columns.tsx
@@ -1,17 +1,10 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { formatDate } from '@/lib/utils'
 import { ColumnDef, FilterFn } from '@tanstack/react-table'
-import { ArrowUpDown, MoreHorizontal } from 'lucide-react'
+import { ArrowUpDown } from 'lucide-react'
+import { ActionCell } from './ActionCell'
 
 const filterProductTypes: FilterFn<OfferType> = (
 	row,
@@ -132,28 +125,7 @@ export const Columns: ColumnDef<OfferType>[] = [
 		header: () => <span>Actions</span>,
 		cell: ({ row }) => {
 			const offer = row.original
-
-			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button variant="ghost" className="h-8 w-8 p-0">
-							<span className="sr-only">Open menu</span>
-							<MoreHorizontal className="h-4 w-4" />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuLabel>Actions</DropdownMenuLabel>
-						<DropdownMenuItem
-							onClick={() => navigator.clipboard.writeText(offer.id)}
-						>
-							Copy offer ID
-						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>View details</DropdownMenuItem>
-						<DropdownMenuItem>Edit offer</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			)
+			return <ActionCell offer={offer} />
 		},
 	},
 ]
